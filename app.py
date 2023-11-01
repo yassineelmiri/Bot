224f8c2dfdf5e486e3d423c88810dbea1a6773eb
Yassine Elmiri <144792335+yassineelmiri@users.noreply.github.com>
2023-11-01 15:02:07 +0100

    Commit on 2023-11-01T15:02:07

--- app.py
+++ app.py
@@ -1,39 +1,40 @@
 import os
-import random
+from random import randint
 import subprocess
 from datetime import datetime, timedelta
 
-# Début du 1er novembre 2023
-start_date = datetime(2023, 11, 15)
-# Fin au 1er janvier 2024
-end_date = datetime(2023, 12, 10)
+# Définir les dates de début et de fin
+start_date = datetime(2023, 11, 1)
+end_date = datetime(2024, 1, 1)
 
+# Boucle pour chaque jour dans la plage de dates spécifiée
 current_date = start_date
-
 while current_date < end_date:
     date_str = current_date.strftime('%Y-%m-%d')
-    random_hour = random.randint(0, 23)
-    random_minute = random.randint(0, 59)
-    random_second = random.randint(0, 59)
-    commit_datetime = datetime(
-        current_date.year,
-        current_date.month,
-        current_date.day,
-        random_hour,
-        random_minute,
-        random_second
-    )
-    commit_date_str = commit_datetime.strftime('%Y-%m-%dT%H:%M:%S')
+    for _ in range(randint(1, 10)):
+        # Générer une heure, minute et seconde aléatoires
+        random_hour = randint(0, 23)
+        random_minute = randint(0, 59)
+        random_second = randint(0, 59)
+        commit_datetime = datetime(
+            current_date.year,
+            current_date.month,
+            current_date.day,
+            random_hour,
+            random_minute,
+            random_second
+        )
+        commit_date_str = commit_datetime.strftime('%Y-%m-%dT%H:%M:%S')
 
-    # Ecrire dans le fichier
-    with open('file.txt', 'a') as file:
-        file.write(f'Commit on {date_str}\n')
+        # Écrire dans le fichier
+        with open('file.txt', 'a') as file:
+            file.write(f'Commit on {commit_date_str}\n')
 
-    # Ajouter les changements
-    subprocess.run(['git', 'add', '.'])
+        # Ajouter les changements
+        subprocess.run(['git', 'add', '.'])
 
-    # Faire le commit avec une date spécifique
-    subprocess.run(['git', 'commit', '--date', commit_date_str, '-m', f'Commit on {date_str}'])
+        # Faire le commit avec une date spécifique
+        subprocess.run(['git', 'commit', '--date', commit_date_str, '-m', f'Commit on {commit_date_str}'])
 
     # Passer au jour suivant
     current_date += timedelta(days=1)
