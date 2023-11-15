8ed6dbcbe5c5e6250e29de613a0e80d11aad6d20
Yassine Elmiri <144792335+yassineelmiri@users.noreply.github.com>
2023-11-15 23:22:49 +0100

    Commit on 2023-11-15

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import subprocess
 from datetime import datetime, timedelta
 
 # Début du 1er novembre 2023
-start_date = datetime(2023, 11, 1)
+start_date = datetime(2023, 11, 15)
 # Fin au 1er janvier 2024
-end_date = datetime(2024, 1, 1)
+end_date = datetime(2023, 12, 10)
 
 current_date = start_date
 
